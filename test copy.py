bfbf04acf2bfa3257ddbc8ac40c819091d201d9b
im-wither <nathanieljwithers@gmail.com>
2026-04-16 11:42:01 -0700

    test commit 2

---
+++ test copy.py
@@ -0,0 +1,8 @@
+# Prompt for name
+name = input("Enter your name: " )
+print("Hello, " + name + ", today is going to be a great day!")
+
+# Prompt for age and add two years
+age = int(input("Enter your age: "))
+future_age = (age + 2)
+print("In two years, you will be", str(future_age) + ".")
